986147703f674f70a4af24e710dfb57f14a19afa
Sandipan Dey <sandipandey.12@gmail.com>
2021-02-07 22:38:50 +0530

    App initials

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => (
+  <div className="container">
+    <div className="aligner">
+      <div className="aligner-item aligner-item-top">{/* <Logo /> */}</div>
+      <div className="aligner-item text-center">{/* <Splash /> */}</div>
+      <div className="aligner-item aligner-item-bottom">{/* <Footer /> */}</div>
+    </div>
+  </div>
+);
+
+export default App;
