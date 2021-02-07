528bd1e1b2fe7d1be79e82f4deeb1818a1381a14
Sandipan Dey <sandipandey.12@gmail.com>
2021-02-07 22:42:52 +0530

    fayah 🔥🔥

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="footer hidden-mobile">
+    <a
+      href="https://github.com/sandipndev/pronounce"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      view source
+    </a>
+    <a
+      href="https://github.com/sandipndev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      © sandipndev,
+    </a>
+    <a
+      href="https://github.com/neilblaze"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      neilblaze,
+    </a>
+    <a
+      href="https://github.com/fias786"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      fias786
+    </a>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Logo.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Logo.css";
+
+class Logo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleReload = this.handleReload.bind(this);
+  }
+
+  handleReload(event) {
+    event.preventDefault();
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <a href="" onClick={this.handleReload} className="logo hidden-mobile">
+        Pronounce!
+      </a>
+    );
+  }
+}
+
+export default Logo;
